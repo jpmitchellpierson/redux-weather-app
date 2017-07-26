f288f3eb7428b1ab59ac39ad12553385c1f43e78
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-07-26 16:13:21 -0700

    bug fixes

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,17 +5,21 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 
 // store and export FETCH_WEATHER variable to keep
 // action types consistent between action creators and reducers
-// used as convention to limit possibility of bugs 
-// caused by typos with a string
+// used as convention to limit possibility of bugs caused by typos
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
+// user enters a city and submits the form
+// action creator function is called
 export function fetchWeather(city) {
+  // store city specific url
   const url = `${ROOT_URL}&q=${city},us`;
-  // store a get request w/ city specific url
+  // get request returns a promise
   const request = axios.get(url);
 
   return {
     type: FETCH_WEATHER,
+    // returning the promise as the payload
+    // redux promise will stop action and return resolved promise
     payload: request
   };
 }
