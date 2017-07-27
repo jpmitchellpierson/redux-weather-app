e45833a179bd978defbc11928d9aed369fcad764
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-07-27 11:47:52 -0700

    added weather charts with units

--- src/containers/weatherList.js
+++ src/containers/weatherList.js
@@ -1,13 +1,22 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
+
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
-    
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp*(9/5) - 459.67);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td><Chart data={temps} color="blue" units="°F" /></td>
+        <td><Chart data={pressures} color="green" units="hPa" /></td>
+        <td><Chart data={humidity} color="red" units="%" /></td>
       </tr>
     );
   }
@@ -18,9 +27,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (°F)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
