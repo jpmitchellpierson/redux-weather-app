4039f61f3cd8f5cb376c7f7fb2e3800c6397ee3d
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-07-27 10:46:03 -0700

    waether list container

--- src/reducers/reducerWeather.js
+++ src/reducers/reducerWeather.js
@@ -1,3 +1,10 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch(action.type) {
+  case FETCH_WEATHER:
+    // create new array and add rest of state elements to it
+    return [ action.payload.data, ...state ];
+  }
   return state;
 }
